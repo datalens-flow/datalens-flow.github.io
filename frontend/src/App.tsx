6433cc7d4f31bf2fc690947aea6e986bfc6f002e
ชวิน โชคชัยมงคลกิจ <chawinccmk.forlearn@gmail.com>
2026-07-18 19:31:57 +0700

    feat: frontend scaffolding and configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '24px' }}>
+      <h1>DataLens is initializing...</h1>
+    </div>
+  );
+}
+
+export default App;
